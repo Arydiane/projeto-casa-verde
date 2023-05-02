3435b2e2b508348dea6e66d8510d70c92282d791
Arydiane <arydianejardim@gmail.com>
2023-05-02 16:27:59 -0300

    atualiza página Homepage adiciona seção como conseguir

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,12 +1,14 @@
 import styles from './HomePage.module.scss'
 import Header from "../../components/Header";
 import NewsletterSubscription from '../../components/NewsletterSubscription';
+import SectionHowToGet from '../../components/SectionHowToGet';
 
 export default function HomePage() {
     return (
         <main>
             <Header />
             <NewsletterSubscription />
+            <SectionHowToGet />
         </main>
     )
 }
