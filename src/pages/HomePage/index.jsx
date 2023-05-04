dac7659609759127dbb0d9d6dcef39b1ab259e65
Arydiane <arydianejardim@gmail.com>
2023-05-04 17:12:13 -0300

    adiciona seção produtos na homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,6 +2,7 @@ import styles from './HomePage.module.scss'
 import Header from "../../components/Header";
 import NewsletterSubscription from '../../components/NewsletterSubscription';
 import SectionHowToGet from '../../components/SectionHowToGet';
+import SectionProducts from '../../components/SectionProducts';
 
 export default function HomePage() {
     return (
@@ -9,6 +10,7 @@ export default function HomePage() {
             <Header />
             <NewsletterSubscription />
             <SectionHowToGet />
+            <SectionProducts />
         </main>
     )
 }
