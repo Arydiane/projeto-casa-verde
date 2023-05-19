63fb7858746ba522b6f6c46a660d1f694f503e37
Arydiane <arydianejardim@gmail.com>
2023-05-18 22:07:51 -0300

    adiciona o envio do e-mail da newsletter

--- src/components/NewsletterSubscription/index.jsx
+++ src/components/NewsletterSubscription/index.jsx
@@ -1,24 +1,26 @@
 import { useState } from 'react'
 import styles from './NewsletterSubscription.module.scss'
+import sendEmail from '../../services/sendEmail'
 
 export default function NewsletterSubscription() {
     const [email, setEmail] = useState('')
-    const [isNotValid, setIsNotValid] = useState (true)
+    const [isNotValid, setIsNotValid] = useState(true)
     const [error, setError] = useState("")
-     
+
     function validateEmail(event) {
         setEmail(event.target.value);
         if (event.target.validity.valid) {
             setIsNotValid(false)
             setError("")
-            return 
-        } 
+            return
+        }
         setError("Informe um e-mail válido. Ex.: email@dominio.com")
     }
 
-    function subscribe(event) {
+    async function subscribe(event) {
         event.preventDefault()
         alert(`Obrigado pela sua assinatura, você receberá nossas novidades no ${email}!`)
+        sendEmail(email)
         setEmail('')
         setIsNotValid(true)
     }
@@ -30,16 +32,16 @@ export default function NewsletterSubscription() {
                 <p className={styles.text}>Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</p>
                 <form className={styles['form']} onSubmit={subscribe}>
                     <div className={styles['form-container']}>
-                        <input 
-                            type="email" 
+                        <input
+                            type="email"
                             placeholder="Insira seu e-mail"
                             value={email}
                             onChange={(event) => validateEmail(event)}
-                            required 
+                            required
                             className={styles['form-input']}
                         />
-                        <button 
-                            type="submit" 
+                        <button
+                            type="submit"
                             disabled={isNotValid}
                             className={styles['form-button']}
                         >
